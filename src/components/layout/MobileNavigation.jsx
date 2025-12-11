b5e2bdccecb7245c6a78f08a16f340b12ea16b9a
guriWiz <guri.developer97@gmail.com>
2025-12-11 10:24:31 +0530

    Removed blog menu from the top header section

--- src/components/layout/MobileNavigation.jsx
+++ src/components/layout/MobileNavigation.jsx
@@ -129,7 +129,7 @@ const MobileNavigation = ({ changeState }) => {
               </ScrollLink>
             )}
           </li>
-          <li className="block">
+          {/* <li className="block">
             <Link href="./posts/1">
               <a
                 className="group relative inline-block cursor-pointer py-2 text-lg uppercase tracking-wider text-heading before:text-primary"
@@ -139,7 +139,7 @@ const MobileNavigation = ({ changeState }) => {
                 <span className="absolute left-0 top-auto bottom-5 inline-block h-px w-full origin-top-right scale-0 bg-primary align-middle transition-transform duration-500 group-hover:origin-top-left group-hover:scale-100"></span>
               </a>
             </Link>
-          </li>
+          </li> */}
           <li className="block">
             {checkroute ? (
               <Link href="/">
